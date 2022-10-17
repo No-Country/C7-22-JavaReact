ae27481e390741b23a43e3fe11cafadd09752496
larrydaguilera <larrydaguilera@gmail.com>
2022-10-17 14:52:12 -0300

    Security Config Fix

--- Backend/CriptoAlgo/src/main/java/com/C722/CriptoAlgo/criptoAlgo/controller/PricesController.java
+++ Backend/CriptoAlgo/src/main/java/com/C722/CriptoAlgo/criptoAlgo/controller/PricesController.java
@@ -1,11 +1,12 @@
 package com.C722.CriptoAlgo.criptoAlgo.controller;
 
 import com.C722.CriptoAlgo.criptoAlgo.auth.utils.CryptoPrice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.C722.CriptoAlgo.criptoAlgo.models.request.WalletUpdateRequest;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +14,12 @@ import java.util.List;
 @RequestMapping("/prices")
 public class PricesController {
 
-    //@GetMapping("/ars")
-    public CryptoPrice getCryptoFiatPrice(String crypto, String fiat){
-        String url ="https://criptoya.com/api/bitso/"+crypto+"/"+fiat+"/1";
+    @GetMapping("/{fiat}/{crypto}")
+    public CryptoPrice getCryptoFiatPrice(@PathVariable("fiat") @Valid @NotNull String fiat,
+                                          @PathVariable("crypto") @Valid @NotNull String crypto)
+
+    {
+        String url = "https://criptoya.com/api/bitso/" + crypto + "/" + fiat + "/1";
         RestTemplate restTemplate = new RestTemplate();
         CryptoPrice prices = restTemplate.getForObject(url, CryptoPrice.class);
         return prices;
